6464712dd9b96c4f9bda6a7c11824988b51e629b
ianreyel <104388366+ianreyel@users.noreply.github.com>
2022-04-25 15:53:23 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>meu primeiro tema</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h1>Olá Mundo</h1>
+	<h2>Meu Primeiro Tema</h2>
+	<h3>Ian reyel Casadei 3MIN</h3>
+</body>
+</html>
